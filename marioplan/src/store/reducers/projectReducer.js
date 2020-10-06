081fea64ce10a24507b922f449965e5b494ddfb8
kendrik <kendrik_lim@artc.a-star.edu.sg>
2020-10-06 10:52:33 +0800

    v14 - using thunk

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,12 +1,16 @@
 const initState = {
-    projects: [
-        {id: '1', title: 'title 1', content: 'content 1'},
-        {id: '2', title: 'title 2', content: 'content 2'},
-        {id: '3', title: 'title 3', content: 'content 3'}
-    ]
+  projects: [
+    { id: "1", title: "title 1", content: "content 1" },
+    { id: "2", title: "title 2", content: "content 2" },
+    { id: "3", title: "title 3", content: "content 3" },
+  ],
 };
 
 const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "CREATE_PROJECT":
+      console.log("created project", action.project);
+  }
   return state;
 };
 
